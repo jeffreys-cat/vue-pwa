53d31e03de389f8b8d3f67416761288e1d0eac27
HuJian <624598206@qq.com>
2017-08-03 18:05:58 +0800

    feat(router): update router

--- src/routers.js
+++ src/routers.js
@@ -1,15 +1,41 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
+import Home from './views/Home.vue';
 import Hello from './views/Hello.vue';
+import NotFound from './views/NotFound.vue';
+import About from './views/About.vue';
+
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello,
-    },
-  ],
+    routes: [
+        {
+            path: '/',
+            component: Home,
+            children: [
+                {
+                    path: 'hello',
+                    alias: '/',
+                    component: Hello,
+                    meta: {
+                        title: 'hello'
+                    }
+                },
+                {
+                    path: 'about',
+                    component: About,
+                    meta: {
+                        title: 'about'
+                    }
+                },
+            ]
+        },
+        // 404
+        {
+            path: '*',
+            component: NotFound,
+        },
+    ],
 });
---
+++ src/views/About.vue
@@ -0,0 +1,3 @@
+<template>
+    <h4>Hello, I'm About Pages</h4>
+</template>
